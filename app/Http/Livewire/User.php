d701f7855280f5c2687b9c91fc8bb521504ff02b
Syed Armash Hussain <45810170+ArmashXD@users.noreply.github.com>
2021-07-09 11:27:58 +0500

    Profile Update

--- app/Http/Livewire/User.php
+++ app/Http/Livewire/User.php
@@ -3,11 +3,31 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\User as ModelUser;
 
 class User extends Component
 {
+    public $email, $name;
+
     public function render()
     {
-        return view('livewire.user.profile', ['user' => auth()->user()]);
+        return view('livewire.user.profile', [$this->email => auth()->user()->email, $this->name => auth()->user()->name]);
     }
+
+    public function update()
+    {
+       $this->validate([
+            'name' => 'required|min:4|max:20',
+            'email' => 'required',
+        ]);
+  
+      $user = ModelUser::find(auth()->id());
+      $user->update([
+        'name' => $this->name,
+        'email' => $this->email,
+      ]);
+
+      session()->flash('success','Updated Successfully');
+    }
+
 }
--- resources/views/livewire/user/profile.blade.php
+++ resources/views/livewire/user/profile.blade.php
@@ -1,10 +1,27 @@
 <div class="card shadow-lg rounded">
-    <form  wire:submit.prevent="update">
+    <form  wire:submit.prevent="update()">
 
     <div class="card-header">
         Your Profile Settings
     </div>
     <div class="card-body">
+        @if (count($errors) > 0)
+        <div class="alert alert-danger">
+            <a href="#" class="close" data-dismiss="alert" style="text-decoration: none; color: white">&times;</a>
+            <strong>Sorry!</strong> invalid input.<br><br>
+            <ul style="list-style-type:none;">
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+      @endif
+      @if(Session::has('success'))
+      <div class="alert alert-success">
+        <div class="close" data-dismiss="alert">&times;</div>
+        <strong>Success </strong>{{ Session::get('success') }}
+      </div>
+    @endif
         <div class="form-group">
             <label for="exampleInputEmail1">Username</label>
             <input type="text" class="form-control" id="exampleInputEmail1" wire:model="name" aria-describedby="emailHelp" placeholder="{{ auth()->user()->name }}" >
