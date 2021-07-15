750dbfaeaa4d952e88b87ca01a105790b910d76c
Syed Armash Hussain <45810170+ArmashXD@users.noreply.github.com>
2021-07-15 12:16:25 +0500

    Added Board seeder

---
+++ database/seeders/BoardTableSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Board;
+class BoardTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $boards = [
+            [
+                'name' => "New"
+            ],
+            [
+                'name' => 'In Progress'
+            ],
+            [
+                'name' => 'Pending'
+            ],
+            [
+                'name' => 'Done'
+            ],
+            [
+                'name' => 'Due',
+            ]
+        ];
+
+        foreach($boards as $board)
+        {
+            Board::create(['title' => $board['name']]);
+        }
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,5 +15,6 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call(PriorityTableSeeder::class);
+        $this->call(BoardTableSeeder::class);
     }
 }
